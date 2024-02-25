57b6f5e1e7009fee91d267cd67ba966d85800611
khairnarhg <hgkhairnar255@outlook.com>
2024-02-25 16:20:00 +0530

    website update 1

--- Main/urls.py
+++ Main/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.index),
     path('login/', views.login),
-    path('signup/', views.signup)
+    path('signup/', views.signup),
+    path('home/', views.home),
+    path('learn/', views.learn),
+    path('about/',views.about)
 ]
--- Main/views.py
+++ Main/views.py
@@ -3,10 +3,20 @@ import requests
 # Create your views here.
 
 def index(request):
-    return render(request, 'Main/index.html')
+    return render(request, 'Main/web.html')
 
 def login(request):
     return render(request, 'Main/login.html')
 
 def signup(request):
     return render(request, 'Main/signup.html')
+
+def learn(request):
+    return render(request, 'Main/learn.html')
+
+def home(request):
+    return render(request , 'Main/home.html')
+
+def about(request):
+    return render(request, 'Main/about.html')
+
